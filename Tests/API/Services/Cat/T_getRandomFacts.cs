651ab2d858130365c5767f20d01cb268abbdefa0
Emil Burman <burman.emil@gmail.com>
2020-03-03 09:42:17 +0100

    Added dependencies

---
+++ Tests/API/Services/Cat/T_getRandomFacts.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTemplate.Tests.API.Services.Cat
+{
+    public class GetRandomFacts : AbstractApiTestcase
+    {
+        [Test]
+        public void validateResponseMatchesHttpcode()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void validateResponseContainsText()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void validateResponseIsRandom()
+        {
+            Assert.Pass();
+        }
+    }
+}
