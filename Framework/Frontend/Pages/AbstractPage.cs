b8ea6a54d99874f315733db8fc1986e22bb652fc
Emil Burman <burman.emil@gmail.com>
2020-03-10 15:54:11 +0100

    Added webdriver skeleton

---
+++ Framework/Frontend/Pages/AbstractPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAutomationFramework.Framework.Frontend.Pages
+{
+    class AbstractPage
+    {
+    }
+}
---
+++ Framework/Frontend/Pages/Wikipedia/Wikipedia_startpage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAutomationFramework.Framework.Frontend.Pages.Wikipedia
+{
+    class Wikipedia_startpage: AbstractPage
+    {
+        
+    }
+}
