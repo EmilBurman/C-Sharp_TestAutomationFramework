35ff973d0975a67084a9f497b6594e813bca5602
Emil Burman <burman.emil@gmail.com>
2020-03-11 13:17:58 +0100

    Update Wikipedia_startpage.cs

--- Framework/Frontend/Pages/Wikipedia/Wikipedia_startpage.cs
+++ Framework/Frontend/Pages/Wikipedia/Wikipedia_startpage.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Ocaramba;
+using Ocaramba.Types;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TestAutomationFramework.Framework.Frontend.Pages.Wikipedia
 {
-    class Wikipedia_startpage: AbstractPage
+    class Wikipedia_startpage : AbstractPage
     {
-        
+        readonly ElementLocator LOGO = new ElementLocator(Locator.Id, "p-logo");
+        readonly ElementLocator SEARCH_FIELD = new ElementLocator(Locator.Id, "searchInput");
+        readonly ElementLocator SEARCH_BUTTON = new ElementLocator(Locator.Id, "searchButton");
+
+        public Wikipedia_startpage(DriverContext driverContext): base(driverContext)
+        {
+
+        }
     }
 }
