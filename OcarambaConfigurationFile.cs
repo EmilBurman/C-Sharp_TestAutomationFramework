f69790026e13d7e08a7161303fee1d25b8e2aaf2
Emil Burman <burman.emil@gmail.com>
2020-03-12 12:28:04 +0100

    Ocaramba skeleton for wikipedia

---
+++ OcarambaConfigurationFile.cs
@@ -0,0 +1,23 @@
+﻿using Ocaramba;
+using Ocaramba.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace TestAutomationFramework
+{
+    public static class OcarambaConfigurationFile
+    {
+        private static readonly string CurrentDirectory = Directory.GetCurrentDirectory();
+
+        /// <summary>
+        /// Gets the Download Folder path
+        /// </summary>
+        public static string DownloadFolderPath
+        {
+            get { return FilesHelper.GetFolder(ConfigurationManager.AppSettings["DownloadFolder"], CurrentDirectory); }
+        }
+    }
+}
