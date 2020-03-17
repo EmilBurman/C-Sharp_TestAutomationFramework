be04da2375cb5bb97b0c73048c670649d15f1aa2
Emil Burman <burman.emil@gmail.com>
2020-03-17 10:41:20 +0100

    Add feature file and steps

---
+++ Tests/Behaviordriven/Features/Omdb/GetItemThroughTitle.feature
@@ -0,0 +1,17 @@
+﻿Feature: GetItemThroughTitle
+	In order to find the item I want
+	As a user of the API
+	I want to search by title
+
+@omdb
+Scenario: Search movie by title
+	Given I have entered the title of the item
+	And I have entered that the type is a movie
+	When I connect to the API
+	Then the response should contain the movie title I searched for
+
+Scenario: Search series by title
+	Given I have entered the title of the item
+	And I have entered that the type is a series
+	When I connect to the API
+	Then the response should contain the series title I searched for
---
+++ Tests/Behaviordriven/Steps/Omdb/GetItemThroughTitleSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TestAutomationFramework.Tests.Behaviordriven.Steps.Omdb
+{
+    [Binding]
+    public class GetItemThroughTitleSteps
+    {
+        [Given(@"I have entered the title of the item")]
+        public void GivenIHaveEnteredTheTitleOfTheItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered that the type is a movie")]
+        public void GivenIHaveEnteredThatTheTypeIsAMovie()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered that the type is a series")]
+        public void GivenIHaveEnteredThatTheTypeIsASeries()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I connect to the API")]
+        public void WhenIConnectToTheAPI()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the response should contain the movie title I searched for")]
+        public void ThenTheResponseShouldContainTheMovieTitleISearchedFor()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the response should contain the series title I searched for")]
+        public void ThenTheResponseShouldContainTheSeriesTitleISearchedFor()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
